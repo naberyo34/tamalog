bbae42b9549db8a117006f9f56f77b817ccc8287
ryo.watanabe <ryo.watanabe@nijibox.co.jp>
2021-05-06 11:55:42 +0900

    add: シェアボタン

---
+++ src/organisms/ShareButtons/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Index from './index';
+
+export default {
+  title: 'organisms/ShareButtons',
+};
+
+export const Default: React.FC = () => <Index url="https://example.com/" />;
