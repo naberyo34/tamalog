8efb36530f25a0c802442195d1ee0c967b9a972e
Ryo <naberyo34@gmail.com>
2022-02-06 17:46:56 +0900

    add: 雑記のサムネ

--- src/services/getThumbnail.ts
+++ src/services/getThumbnail.ts
@@ -1,5 +1,6 @@
 import thumbProgramming from '@/images/thumbnail_programming.png';
 import thumbIllust from '@/images/thumbnail_illust.png';
+import diaryIllust from '@/images/thumbnail_diary.png';
 
 /**
  * tagsに含まれるタグを調べて、対応するサムネイル画像を返す
@@ -10,6 +11,7 @@ const getThumbnail = (tags?: readonly GatsbyTypes.Maybe<string>[]): string => {
   if (!tags || tags.length === 0) return '';
   if (tags.includes('プログラミング')) return thumbProgramming;
   if (tags.includes('イラスト')) return thumbIllust;
+  if (tags.includes('雑記')) return diaryIllust;
 
   return '';
 };
